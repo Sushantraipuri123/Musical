79b3c8f893cba42aac0929aa3f43884a3659c896
Sushant Raipuri <cashu853@gmail.com>
2024-09-18 17:13:41 +0530

    changed button in all courses page

--- src/components/courses/CoursesCards.tsx
+++ src/components/courses/CoursesCards.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import { CardBody, CardContainer, CardItem } from "../ui/3d-card";
 import Link from "next/link";
 import courseData from "@/data/music_courses.json";
-
+import { HoverBorderGradient } from "../ui/hover-border-gradient";
 // making custom data type
 interface Course {
   id: number;
@@ -18,57 +18,59 @@ interface Course {
 }
 export function FeaturesSectionDemo() {
   return (
-   <>
-     <div className=" flex flex-wrap gap-1 lg:gap-8 xl:gap-10 mx-2 lg:mx-4 xl:mx-8 justify-center">
-      {courseData.courses.map((course : Course) =>(
-              <CardContainer key={course.id}>
-                  <CardBody
-                className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border"
+    <>
+      <div className=" flex flex-wrap gap-1 lg:gap-8 xl:gap-10 mx-2 lg:mx-4 xl:mx-8 justify-center">
+        {courseData.courses.map((course: Course) => (
+          <CardContainer key={course.id}>
+            <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border">
+              <CardItem
+                translateZ="50"
+                className="text-xl font-bold text-neutral-600 dark:text-white"
               >
-                <CardItem
-                  translateZ="50"
-                  className="text-xl font-bold text-neutral-600 dark:text-white"
+                {course.title}
+              </CardItem>
+              <CardItem
+                as="p"
+                translateZ="60"
+                className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+              >
+                {course.description}
+              </CardItem>
+              <CardItem translateZ="100" className="w-full mt-4">
+                <Image
+                  src={course.image}
+                  height="1000"
+                  width="1000"
+                  className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                  alt={course.title}
+                />
+              </CardItem>
+              <div className="flex justify-between items-center mt-20">
+                <CardItem>   
+                 
+                  <Link  href={`/cources/${course.slug}`}>
+                <HoverBorderGradient
+                  containerClassName="rounded-full"
+                  as="button"
+                  className="dark:bg-black bg-white text-black dark:text-white flex items-center space-x-2"
                 >
-                  {course.title}
+                  <span> Learn More →</span>
+                </HoverBorderGradient>
+                </Link>
                 </CardItem>
+                
                 <CardItem
-                  as="p"
-                  translateZ="60"
-                  className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+                  translateZ={20}
+                  as="button"
+                  className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
                 >
-                  {course.description}
+                  ${course.price}
                 </CardItem>
-                <CardItem translateZ="100" className="w-full mt-4">
-                  <Image
-                    src={course.image}
-                    height="1000"
-                    width="1000"
-                    className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
-                    alt={course.title}
-                  />
-                </CardItem>
-                <div className="flex justify-between items-center mt-20">
-                  <CardItem
-                    translateZ={20}
-                    as={Link}
-                    href={`/cources/${course.slug}`}
-                    className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
-                  >
-                    Learn More →
-                  </CardItem>
-                  <CardItem
-                    translateZ={20}
-                    as="button"
-                    className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
-                  >
-                    ${course.price}
-                  </CardItem>
-                </div>
-              </CardBody>
-              </CardContainer>
-      ))}
-    </div>
-   </>
+              </div>
+            </CardBody>
+          </CardContainer>
+        ))}
+      </div>
+    </>
   );
 }
-
